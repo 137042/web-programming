2ff89eb5f0061a2cbab8c5ee99ce39ecaa367c07
서해 <mare137042@gmail.com>
2022-04-18 21:55:54 +0900

    unfinished - javascript practice

--- js/js_01_script.js
+++ js/js_01_script.js
@@ -8,6 +8,11 @@ function unfold(){
 }
 
 function fold(){
-
+    let open = document.getElementsByClassName("open")[0];
+    let menu = document.getElementsByClassName("menu")[0];
+    menu.addEventListener("click", function(){
+        open.classList.remove("hidden");
+        menu.classList.add("hidden");
+    })
 }
 
