8a0dd67ab8ac4b7c58e0c4d851d473dfde22b250
서해 <mare137042@gmail.com>
2022-04-19 21:39:28 +0900

    finished - javascript practice

--- js/js_01_script.js
+++ js/js_01_script.js
@@ -5,11 +5,6 @@ window.onload = () => {
         acc[i].addEventListener("click", panelControl);
     }
 
-    var btnR = document.getElementsByClassName("remove");
-    for(i = 0; i < btnR.length; i++){
-        btnR[i].addEventListener("click", remove);
-    }
-
     var btnP = document.getElementsByClassName("plus");
     for(i = 0; i < btnP.length; i++){
         btnP[i].addEventListener("click", plus);
@@ -18,6 +13,11 @@ window.onload = () => {
     var btnM = document.getElementsByClassName("minus");
     for(i = 0; i < btnM.length; i++){
         btnM[i].addEventListener("click", minus);
+    }
+
+    var btnR = document.getElementsByClassName("remove");
+    for(i = 0; i < btnR.length; i++){
+        btnR[i].addEventListener("click", remove);
     }
 }
 
@@ -39,6 +39,27 @@ function fold(){
     })
 }
 
+let slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.add("hidden");
+
+    }
+    slides[slideIndex - 1].classList.remove("hidden");
+}
+
 function panelControl(event){
     var panel = event.target.nextElementSibling;
     this.classList.toggle("active");
@@ -51,61 +72,106 @@ function show(){
     option.classList.remove("hidden");
 }
 
-var totalValue = 0;
-
 function plus(){
     const option = this.parentNode;
-    const count = this.nextElementSibling.nextElementSibling.nextElementSibling;
-    const tag = this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
+    var count = this.nextElementSibling.nextElementSibling.nextElementSibling;
+    var tag = this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
+    var total = document.getElementsByClassName("total-price")[0];
+    
     var cnt = parseInt(count.innerHTML);
     var value = parseInt(tag.innerHTML);
+    var totalValue = parseInt(total.innerHTML);
 
-    if(option.value == "tent-A"){
+    if(option.classList[0] == "tent-A"){
         cnt = cnt + 1;
         value = value + 39800;
         totalValue = totalValue + 39800;
     }
-    if(option.value == "tent-B"){
+    if(option.classList[0] == "tent-B"){
         cnt = cnt + 1;
         value = value + 10000;
         totalValue = totalValue + 10000;
     }
-    if(option.value == "tent-C"){
+    if(option.classList[0] == "tent-C"){
         cnt = cnt + 1;
         value = value + 20000;
         totalValue = totalValue + 20000;
     }
+
+    count.textContent = String(cnt);
+    tag.textContent = String(value);
+    total.textContent = String(totalValue);
 }
 
 function minus(){
     const option = this.parentNode;
-    const count = this.nextElementSibling.nextElementSibling.nextElementSibling;
-    const tag = this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
+    var count = this.nextElementSibling.nextElementSibling;
+    var tag = this.nextElementSibling.nextElementSibling.nextElementSibling;
+    var total = document.getElementsByClassName("total-price")[0];
+    
     var cnt = parseInt(count.innerHTML);
     var value = parseInt(tag.innerHTML);
+    var totalValue = parseInt(total.innerHTML);
 
-    if(option.value == "tent-A"){
-        cnt = cnt - 1;
-        value = value - 39800;
-        totalValue = totalValue - 39800;
-    }
-    if(option.value == "tent-B"){
-        cnt = cnt - 1;
-        value = value - 10000;
-        totalValue = totalValue - 10000;
-    }
-    if(option.value == "tent-C"){
-        cnt = cnt - 1;
-        value = value - 20000;
-        totalValue = totalValue - 20000;
+    if(cnt != 0){
+        if(option.classList[0] == "tent-A"){
+            cnt = cnt - 1;
+            value = value - 39800;
+            totalValue = totalValue - 39800;
+        }
+        if(option.classList[0] == "tent-B"){
+            cnt = cnt - 1;
+            value = value - 10000;
+            totalValue = totalValue - 10000;
+        }
+        if(option.classList[0] == "tent-C"){
+            cnt = cnt - 1;
+            value = value - 20000;
+            totalValue = totalValue - 20000;
+        }
+        count.textContent = String(cnt);
+        tag.textContent = String(value);
+        total.textContent = String(totalValue);
     }
 }
 
 function remove(){
     var option = this.parentNode;
-    const count = this.nextElementSibling.nextElementSibling.nextElementSibling;
-    const tag = this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
+    option.classList.add("hidden");
+
+    var count = this.nextElementSibling;
+    var tag = this.nextElementSibling.nextElementSibling;
+    var total = document.getElementsByClassName("total-price")[0];
     var cnt = parseInt(count.innerHTML);
     var value = parseInt(tag.innerHTML);
+    var totalValue = parseInt(total.innerHTML);
 
+    totalValue = totalValue - value;
+    value = cnt == 0
+
+    count.textContent = String(cnt);
+    tag.textContent = String(value);
+    total.textContent = String(totalValue);
+}
+
+function buy(){
+    var cnts = document.getElementsByClassName("count");
+    var tags = document.getElementsByClassName("tag");
+    var price = document.getElementsByClassName("total-price")[0];
+    
+    for(var i = 0; i < cnts.length; i++){
+        cnts[i].innerHTML.replace("0");
+        tags[i].innerHTML.replace("0");
     }
+    price.textContent = "0";
+
+    var option = document.getElementsByClassName("tent-A")[0];
+    option.classList.add("hidden");
+    option = document.getElementsByClassName("tent-B")[0];
+    option.classList.add("hidden");
+    option = document.getElementsByClassName("tent-C")[0];
+    option.classList.add("hidden");
+
+    var selection = document.getElementById("tents")
+    selection.options[0].selected = "selected";
+}
