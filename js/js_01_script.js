00b8c1d4a563dc7313f179946150a9fd16e9b3bd
서해 <mare137042@gmail.com>
2022-04-19 15:21:47 +0900

    unfinished - javascript practice

--- js/js_01_script.js
+++ js/js_01_script.js
@@ -1,3 +1,26 @@
+window.onload = () => {
+    var i;
+    var acc = document.getElementsByClassName("title");
+    for(i = 0; i < acc.length; i++){
+        acc[i].addEventListener("click", panelControl);
+    }
+
+    var btnR = document.getElementsByClassName("remove");
+    for(i = 0; i < btnR.length; i++){
+        btnR[i].addEventListener("click", remove);
+    }
+
+    var btnP = document.getElementsByClassName("plus");
+    for(i = 0; i < btnP.length; i++){
+        btnP[i].addEventListener("click", plus);
+    }
+
+    var btnM = document.getElementsByClassName("minus");
+    for(i = 0; i < btnM.length; i++){
+        btnM[i].addEventListener("click", minus);
+    }
+}
+
 function unfold(){
     let open = document.getElementsByClassName("open")[0];
     let menu = document.getElementsByClassName("menu")[0];
@@ -16,16 +39,73 @@ function fold(){
     })
 }
 
-$(".title").on("click", function() {
-
-function slideDown(){
-    $(".info").addClass("hidden");
-    $(this).next().removeClass("hidden");
+function panelControl(event){
+    var panel = event.target.nextElementSibling;
+    this.classList.toggle("active");
+    panel.classList.toggle("hidden");
 }
 
-function slideUp(){
-    $(this).next().addClass("hidden");
+function show(){
+    const name = document.querySelector("#tents").value;
+    var option = document.getElementsByClassName(name)[0];
+    option.classList.remove("hidden");
 }
 
-$(this).next().hasClass("hidden") ? slideDown() : slideUp();
-});
+var totalValue = 0;
+
+function plus(){
+    const option = this.parentNode;
+    const count = this.nextElementSibling.nextElementSibling.nextElementSibling;
+    const tag = this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
+    var cnt = parseInt(count.innerHTML);
+    var value = parseInt(tag.innerHTML);
+
+    if(option.value == "tent-A"){
+        cnt = cnt + 1;
+        value = value + 39800;
+        totalValue = totalValue + 39800;
+    }
+    if(option.value == "tent-B"){
+        cnt = cnt + 1;
+        value = value + 10000;
+        totalValue = totalValue + 10000;
+    }
+    if(option.value == "tent-C"){
+        cnt = cnt + 1;
+        value = value + 20000;
+        totalValue = totalValue + 20000;
+    }
+}
+
+function minus(){
+    const option = this.parentNode;
+    const count = this.nextElementSibling.nextElementSibling.nextElementSibling;
+    const tag = this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
+    var cnt = parseInt(count.innerHTML);
+    var value = parseInt(tag.innerHTML);
+
+    if(option.value == "tent-A"){
+        cnt = cnt - 1;
+        value = value - 39800;
+        totalValue = totalValue - 39800;
+    }
+    if(option.value == "tent-B"){
+        cnt = cnt - 1;
+        value = value - 10000;
+        totalValue = totalValue - 10000;
+    }
+    if(option.value == "tent-C"){
+        cnt = cnt - 1;
+        value = value - 20000;
+        totalValue = totalValue - 20000;
+    }
+}
+
+function remove(){
+    var option = this.parentNode;
+    const count = this.nextElementSibling.nextElementSibling.nextElementSibling;
+    const tag = this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
+    var cnt = parseInt(count.innerHTML);
+    var value = parseInt(tag.innerHTML);
+
+    }
